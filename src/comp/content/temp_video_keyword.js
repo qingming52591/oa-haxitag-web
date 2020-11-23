21f724ee2603bfb6e35f958098e20f4a29fc1ff7
daypop-zq <qiang@daypop.ai>
2020-11-23 18:43:00 +0800

    keyword

--- src/comp/content/temp_video_keyword.js
+++ src/comp/content/temp_video_keyword.js
@@ -1,10 +1,25 @@
-import {Col, Row, Button, Form, Input, message, Modal, Table, Space, Avatar} from "antd";
+import {
+    Col,
+    Row,
+    Button,
+    Form,
+    Input,
+    DatePicker,
+    Modal,
+    Table,
+    Space,
+    Avatar,
+    Select,
+    Checkbox,
+    InputNumber
+} from "antd";
 import * as event from "../../event";
 import React from 'react'
 import {Link} from "react-router-dom";
 
 
 export const VideoKeyword = (props) => {
+    const [showModal, setShowModal] = React.useState(false)
     const columns = [
         {
             title: '关键词',
@@ -32,7 +47,9 @@ export const VideoKeyword = (props) => {
             render: (text, record) => {
                 return (<>
                     <Space>
-                        <Link>编辑</Link>
+                        <Button type="link" onClick={e => {
+                            setShowModal(true)
+                        }}>编辑</Button>
                     </Space>
                 </>)
             }
@@ -41,7 +58,10 @@ export const VideoKeyword = (props) => {
     return (
         <>
             <Row justify="end">
-                <Button type={'primary'}>添加</Button>
+                <EditVideoKeyword show={showModal} setShow={setShowModal}></EditVideoKeyword>
+                <Button type={'primary'} onClick={e => {
+                    setShowModal(true)
+                }}>添加</Button>
             </Row>
             <Table
                 columns={columns}
@@ -127,6 +147,64 @@ export const VideoKeyword = (props) => {
                     }
                 }}
             />
+        </>
+    )
+}
+
+
+const EditVideoKeyword = (props) => {
+    const [form] = Form.useForm()
+    return (
+        <>
+            <Modal
+                title={'编辑'}
+                visible={props.show}
+                okText={'保存'} cancelText={'取消'}
+                onCancel={(e) => {
+                    props.setShow(false)
+                    form.resetFields()
+                }}
+                onOk={() => {
+                    props.setShow(false)
+                }}
+            >
+                <Form form={form}>
+                    <Form.Item label={"关键词"}>
+                        <Input/>
+                    </Form.Item>
+                    <Form.Item label={"分类"}>
+                        <Select>
+                            <Select.Option value={"Travel"}>Travel</Select.Option>
+                            <Select.Option value={"Tech"}>Tech</Select.Option>
+                            <Select.Option value={"Sports"}>Sports</Select.Option>
+                            <Select.Option value={"Music"}>Music</Select.Option>
+                            <Select.Option value={"How to"}>How to</Select.Option>
+                            <Select.Option value={"Games"}>Games</Select.Option>
+                            <Select.Option value={"Movie"}>Movie</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label={"时长"}>
+                        <Select>
+                            <Select.Option value={"≤5min"}>≤5min</Select.Option>
+                            <Select.Option value={"≤10min"}>≤10min</Select.Option>
+                            <Select.Option value={"≤20min"}>≤20min</Select.Option>
+                            <Select.Option value={"≤30min"}>≤30min</Select.Option>
+                            <Select.Option value={"≤60min"}>≤60min</Select.Option>
+                            <Select.Option value={"≤120min"}>≤120min</Select.Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item label={"清晰度"}>
+                        <Checkbox.Group options={[
+                            {label: '1080P', value: '1080P'},
+                            {label: '720P', value: '720P'},
+                            {label: '480P', value: '480P'},
+                            {label: '360P', value: '360P'},
+                            {label: '240P', value: '240P'},
+                            {label: '144P', value: '144P'},
+                        ]}></Checkbox.Group>
+                    </Form.Item>
+                </Form>
+            </Modal>
         </>
     )
 }
