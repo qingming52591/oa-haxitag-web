cf4b5731ad2e5aa5cf7f7f255c3b1aa48395fa33
daypop-zq <qiang@daypop.ai>
2021-05-10 17:01:53 +0800

    视频路径

--- src/conf/config.mini.js
+++ src/conf/config.mini.js
@@ -5,7 +5,7 @@ const config = {
     API_OA_PREFIX: API_OA_PREFIX,
     API_OA_HOST: API_OA_HOST,
     API_OA_BASE: `${API_OA_HOST}/${API_OA_PREFIX}`,
-    VIDEO_BASE: `${API_OA_HOST}`,
+    VIDEO_BASE: 'http://sckm.vip:30000',
     TITLE: '阅粒智能知识系统',
     DEBUG: false
 }
