a5dcaf3ac6893e2e0ad2779dc629aafa7ef807fd
郭清铭 <guoqingming@6clue.com>
2022-02-08 20:48:36 +0800

    dele-mini

--- src/conf/config.mini.js
+++ src/conf/config.mini.js
@@ -1,11 +1,11 @@
 let API_OA_PREFIX = 'oa-haxitag'
-let API_OA_HOST = 'http://sckm.vip'
+let API_OA_HOST = 'http://120.132.42.97'
 
 const config = {
     API_OA_PREFIX: API_OA_PREFIX,
     API_OA_HOST: API_OA_HOST,
     API_OA_BASE: `${API_OA_HOST}/${API_OA_PREFIX}`,
-    VIDEO_BASE: 'http://sckm.vip',
+    VIDEO_BASE: 'http://120.132.42.97',
     TITLE: '阅粒智能知识系统',
     DEBUG: false
 }
