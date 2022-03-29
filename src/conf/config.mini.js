2f63e7b3e06c2f0cba403352f1a9a297a8be4796
郭清铭 <guoqingming@6clue.com>
2022-03-29 12:43:06 +0800

    1.0

--- src/conf/config.mini.js
+++ src/conf/config.mini.js
@@ -1,11 +1,11 @@
 let API_OA_PREFIX = 'oa-haxitag'
-let API_OA_HOST = 'http://120.132.42.97'
+let API_OA_HOST = 'https://km.yueli.com'
 
 const config = {
     API_OA_PREFIX: API_OA_PREFIX,
     API_OA_HOST: API_OA_HOST,
     API_OA_BASE: `${API_OA_HOST}/${API_OA_PREFIX}`,
-    VIDEO_BASE: 'http://120.132.42.97',
+    VIDEO_BASE: 'https://km.yueli.com',
     TITLE: '阅粒智能知识系统',
     DEBUG: false
 }
