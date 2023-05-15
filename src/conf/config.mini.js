a2262a3afa555e80fe571505934db5335116a73b
郭清铭-chainupeye <guoqingming@haxitag.com>
2023-05-15 16:20:50 +0800

    1

--- src/conf/config.mini.js
+++ src/conf/config.mini.js
@@ -1,6 +1,6 @@
 let API_OA_PREFIX = 'oa-haxitag'
 let API_OA_HOST = 'https://op.dekolla.xyz/api'
-let API_MANAGE_HOST = 'http://43.154.145.166:8800'
+let API_MANAGE_HOST = 'https://bubbie.dekolla.xyz'
 const config = {
     API_OA_PREFIX: API_OA_PREFIX,
     API_OA_HOST: API_OA_HOST,
